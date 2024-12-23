d2c2058776450b1c1794b8ffd292df905c74bfde
SadiaMs <162547312+SadiaMs@users.noreply.github.com>
2024-12-23 19:51:48 +0500

    Update TopHeader.tsx
    link add karme k liye

--- src/components/TopHeader.tsx
+++ src/components/TopHeader.tsx
@@ -3,24 +3,22 @@ import { FaRegHeart } from "react-icons/fa";
 import { RiShoppingCart2Line } from "react-icons/ri";
 import { IoIosSearch } from "react-icons/io";
 import { BsPersonPlus } from "react-icons/bs";
+import Link from 'next/link';
 
 const TopHeader = () => {
   return (
     <header className="text-gray-600 body-font">
-      <div className="container bg-[#0000000] mx-auto flex flex-wrap  w-[95%] 2xl :w-[100%] p-5 flex-col md:flex-row items-center">
-      
-
-        {/* Navigation Links . .... */}
-        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-          <a className  ="mr-5 hover:text-gray-900">Home</a>
-          <a className="mr-5 hover:text-gray-900">Shop</a>
-          <a className="mr-5 hover:text-gray-900">About</a>
-          <a className="mr-5 hover:text-gray-900">Contact</a>
+      <div className="container bg-[#0000000] mx-auto flex flex-wrap w-[95%] 2xl:w-[100%] p-5 flex-col md:flex-row items-center">
+        {/* Navigation Links */}
+        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center cursor-pointer">
+          <Link href="/" className="mr-5 hover:text-gray-900">Home</Link>
+          <Link href="/Shop" className="mr-5 hover:text-gray-900">Shop</Link>
+          <Link href="/About" className="mr-5 hover:text-gray-900">About</Link>
+          <Link href="/Contact" className="mr-5 hover:text-gray-900">Contact</Link>
         </nav>
 
-        {/* Icon Section    ...*/}
+        {/* Icon Section */}
         <div className="flex items-center text-xl space-x-4">
-         
           <BsPersonPlus className="hover:text-gray-900 cursor-pointer" />
           <IoIosSearch className="hover:text-gray-900 cursor-pointer" />
           <FaRegHeart className="hover:text-gray-900 cursor-pointer" />
@@ -32,3 +30,4 @@ const TopHeader = () => {
 };
 
 export default TopHeader;
+
